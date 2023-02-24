20aef0983eb81baea3ea2c1a551b1125519d191f
djpomidor <djpomidor@gmail.com>
2023-02-24 23:19:57 +0300

    save

--- backend/finalproject/printery/serializers.py
+++ backend/finalproject/printery/serializers.py
@@ -2,7 +2,12 @@ from rest_framework import serializers
 from .models import *
 
 class UserSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = User 
         fields = ('pk','username', 'first_name', 'last_name', 'email', 'phone_number', 'is_customer', 'is_employee')
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order 
+        fields = ('pk','number', 'name', 'owner', 'type', 'circulation', 'binding', 'width', 'height', 'block', 'cover', 'insert', 'created', 'due_date', 'delivery_date')
--- backend/finalproject/printery/urls.py
+++ backend/finalproject/printery/urls.py
@@ -4,18 +4,24 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path("", views.index, name="index"),
+#     path("login", views.login_view, name="login"),
+#     path("logout", views.logout_view, name="logout"),
+#     path("register", views.register, name="register"),
+#     path("user-cabinet", views.user_cabinet_view, name="user-cabinet"),
+#     path("create-order", views.create_order, name="create-order"),
+#     path("manage", views.manage, name="manage"),
+#     path("print-schedule", views.print_schedule, name="print-schedule"),
+
+#     #api root
+#     path("orders/<int:order_number>", views.orders_view, name="orders-view"),
+#     path("orders/all", views.orders_view, name="orders-view"),
+# ]
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.index, name="index"),
-    path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register"),
-    path("user-cabinet", views.user_cabinet_view, name="user-cabinet"),
-    path("create-order", views.create_order, name="create-order"),
-    path("manage", views.manage, name="manage"),
-    path("print-schedule", views.print_schedule, name="print-schedule"),
-
-    #api root
-    path("orders/<int:order_number>", views.orders_view, name="orders-view"),
-    path("orders/all", views.orders_view, name="orders-view"),
-]
+    re_path(r'^api/users/$', views.users_list),
+    re_path(r'^api/users/(\d+)$', views.users_detail),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
