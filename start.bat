661e1f9a4b7d3539801d0ee7e174993fb34a905c
djpomidor <djpomidor@gmail.com>
2025-07-29 14:32:24 +0300

    update docker-compose.yml

--- start.bat
+++ start.bat
@@ -1,10 +1,27 @@
 cd backend
 .\.venv\Scripts\activate     
 python manage.py runserver
-
 venv\Scripts\activate  
 
 docker-compose build frontend
+
+
+перезапустить:
 docker-compose up -d
 
+
 docker logs --since 5m backend
+
+Зайди внутрь контейнера backend:
+docker exec -it backend bash
+
+открыт ли порт 8000:
+sudo ufw status
+
+
+Удалить ненужные пакеты, логи и старые Docker-образы:
+apt-get clean
+docker system prune -af
+
+Подключись к контейнеру базы данных:
+docker exec -it postgres_db psql -U postgres
